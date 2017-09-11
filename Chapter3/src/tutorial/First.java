b764164d9d869bd72fad95de74b9b20366a3c271
Braden031401 <rc179978@students.davenport.k12.ia.us>
2017-09-11 09:45:37 -0500

    First! Second!

---
+++ Chapter3/src/tutorial/First.java
@@ -0,0 +1,21 @@
+package tutorial;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("What is your address");
+		displayAddress();
+		// Will call for display address
+		
+	}
+	public static void displayAddress()
+	{
+		System.out.println("XYZ Company");
+		System.out.println("8900 U.S. Hwy 14");
+		System.out.println("Crystal Lake, IL 60014");
+		System.out.println("First Java Application");
+	}
+
+}
---
+++ Chapter3/src/tutorial/Second.java
@@ -0,0 +1,13 @@
+package tutorial;
+
+public class Second {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		First.displayAddress();
+		
+		
+	}
+
+}
