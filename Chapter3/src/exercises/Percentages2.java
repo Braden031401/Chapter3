18c6c704628782fc47494fc913e85e11d1c1e393
Braden031401 <rc179978@students.davenport.k12.ia.us>
2017-09-15 08:36:48 -0500

    Percentages

--- Chapter3/src/exercises/Percentages2.java
+++ Chapter3/src/exercises/Percentages2.java
@@ -1,12 +1,19 @@
 package exercises;
 
-public class Percentages {
+import java.util.Scanner;
+
+public class Percentages2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		double num1 = 10;
-		double num2 = 20;
+		double num1;
+		double num2;
+		Scanner input = new Scanner(System.in);
 		
+		System.out.print("Please Specify Number 1 >>> ");
+		num1 = input.nextInt();
+		System.out.print("Please Specify Number 2 >>> ");
+		num2 = input.nextInt();
 		
 		computePercent(num1, num2);
 }
