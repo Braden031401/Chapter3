ed8eff9239efb85c6147250062a2e5c757cf3ab3
Braden031401 <rc179978@students.davenport.k12.ia.us>
2017-09-15 09:19:25 -0500

    Chapter3

--- Chapter3/src/exercises/DelgadosTacos.java
+++ Chapter3/src/exercises/DelgadosTacos.java
@@ -5,6 +5,38 @@ public class DelgadosTacos {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
-
+	System.out.print(displayMenu);
+		
+		
+		
+		displayMenu();
+		itemsOrdered();
 }
+public static void displayMenu()
+{
+	double Taco = 1.50;
+	double VeggieTaco = 1.50;
+	double Quesadilla = 3.00;
+	double Burrito = 2.00;
+	double AddHotSauce = .50;
+	double Water = .50;
+	double Soda = 1.50;
+	double Chips = 1.00;
+	double Churros = 1.00;
+	double FruitPie = 2.00;
+}
+public static void itemsOrdered()
+{
+	double Taco;
+	double VeggieTaco;
+	double Quesadilla;
+	double Burrito;
+	double AddHotSauce;
+	double Water;
+	double Soda;
+	double Chips;
+	double Churros;
+	double FruitPie;
+	}
+}
+
