d251505382569384b9182c427a6cb1ac72c07af4
Braden031401 <rc179978@students.davenport.k12.ia.us>
2017-09-15 09:30:17 -0500

    Dis is gr8

--- Chapter3/src/exercises/DelgadosTacos.java
+++ Chapter3/src/exercises/DelgadosTacos.java
@@ -1,16 +1,19 @@
 package exercises;
-
+import java.util.Scanner;
 public class DelgadosTacos {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	System.out.print(displayMenu);
+	System.out.println("What do you want to eat?");
+	possibleOrders();
+	Scanner input = new Scanner(System.in);
 		
 		
 		
 		displayMenu();
-		itemsOrdered();
+	
+		
 }
 public static void displayMenu()
 {
@@ -25,18 +28,41 @@ public static void displayMenu()
 	double Churros = 1.00;
 	double FruitPie = 2.00;
 }
-public static void itemsOrdered()
+public static void possibleOrders()
 {
-	double Taco;
-	double VeggieTaco;
-	double Quesadilla;
-	double Burrito;
-	double AddHotSauce;
-	double Water;
-	double Soda;
-	double Chips;
-	double Churros;
-	double FruitPie;
+	Scanner input = new Scanner(System.in);
+	
+	double Taco = 1.50;
+	double VeggieTaco = 1.50;
+	double Quesadilla = 3.00;
+	double Burrito = 2.00;
+	double AddHotSauce = .50;
+	double Water = .50;
+	double Soda = 1.50;
+	double Chips = 1.00;
+	double Churros = 1.00;
+	double FruitPie = 2.00;
+	
+	System.out.println("Taco = 1.50");
+	Taco = input.nextDouble();
+	System.out.println("VeggieTaco = 1.50");
+	VeggieTaco = input.nextDouble();
+	System.out.println("Quesadilla = 3.00");
+	Quesadilla = input.nextDouble();
+	System.out.println("Burrito = 2.00");
+	Burrito = input.nextDouble();
+	System.out.println("AddHotSauce = .50");
+	AddHotSauce = input.nextDouble();
+	System.out.println("Water = .50");
+	Water = input.nextDouble();
+	System.out.println("Soda = 1.50");
+	Soda = input.nextDouble();
+	System.out.println("Chips = 1.00");
+	Chips = input.nextDouble();
+	System.out.println("Churros = 1.00");
+	Churros = input.nextDouble();
+	System.out.println("FruitPie = 2.00");
+	FruitPie = input.nextDouble();
 	}
 }
 
